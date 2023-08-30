6e1979748722ce76bfae7f1707d5e8224131b56f
huhuang03 <huhuang03@gmail.com>
2023-08-31 00:58:52 +0800

    up

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,31 @@
 // Created by huhua on 2023/8/30.
 //
 #include <iostream>
+#include <boost/filesystem.hpp>
+#include <reader_util/reader_util.h>
 
-int main() {
+namespace fs = boost::filesystem;
+namespace ru = reader_util;
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cout << "Usage: file-reader font-file-path" << std::endl;
+    return -1;
+  }
+
+
+  std::string fontFilePath = std::string(argv[1]);
+  if (fs::exists(fontFilePath)) {
+    std::cout << fontFilePath << " not exists" << std::endl;
+    return -1;
+  }
+
+  if (fs::is_directory(fontFilePath)) {
+    std::cout << fontFilePath << " is directory, should be a file." << std::endl;
+    return -1;
+  }
+
+
   std::cout << "hello font" << std::endl;
   return 0;
 }
